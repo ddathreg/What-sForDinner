0af720923d57f770325821488ab48481c488ed43
Nathan Huang <nathanhuang34@gmail.com>
2025-06-05 12:49:26 -0700

    s

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -13,7 +13,12 @@ dotenv.config();
 const app = express();
 const port = 8000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://ambitious-tree-0c41c301e.6.azurestaticapps.net",
+    credentials: true,
+  }),
+);
 // Add limits to express.json middleware
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
